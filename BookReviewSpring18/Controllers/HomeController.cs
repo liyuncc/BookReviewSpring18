d8b7ac409b0dceb300e5e946aa9176e328f711f4
liyun <liyuncecil@gmail.com>
2018-04-17 11:42:15 -0700

    Modify Home example

--- BookReviewSpring18/Controllers/HomeController.cs
+++ BookReviewSpring18/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookReviewSpring18.Models;
 
 namespace BookReviewSpring18.Controllers
 {
@@ -10,7 +11,8 @@ namespace BookReviewSpring18.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            BookReviewDbEntities db = new BookReviewDbEntities();
+            return View(db.Categories.ToList());
         }
 
         public ActionResult About()
